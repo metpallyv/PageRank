02304eb0b1cd051c8ca8329d3f0a97d5157c06a7
Vardhaman Metpally <vardhaman.metpally@gmail.com>
2015-09-30 18:46:47 -0700

    Update PageRank.py

--- PageRank.py
+++ PageRank.py
@@ -13,6 +13,8 @@ SortedPR = []
 
 d = 0.85
 
+#parse the file and create a inlink dictionary, list of sink nodes, outlink dictionary
+#initial Page rank dictionary
 def parse_file(inlink):
     #input = open('./inlink1.txt', 'r')
     input = open(inlink, 'r')
@@ -21,33 +23,34 @@ def parse_file(inlink):
         words = string.split(line)
         inlink_dict[words[0]] = words[1:]
         Pages.append(words[0])
-
     len_dic = float(len(Pages))
     for page in Pages:
         PRank[page] = float(1) / len_dic
-
     for page in inlink_dict.keys():
         for q in inlink_dict[page]:
             if outlink_dict.has_key(q):
                 outlink_dict[q] += 1
             else:
                 outlink_dict[q] = 1
-
     for page in inlink_dict.keys():
         if not outlink_dict.has_key(page):
             sinknodes.append(page)
-    print (len(sinknodes))
+    #print (len(sinknodes))
+    
+#Page rank dump    
 def dump_pagerank():
     print("Dumping the page rank:")
     for page in PRank.keys():
        print ("Page Rank for the Page",page,"is",PRank.get(page))
 
+#Perplexity calculation
 def cal_Perplexity():
     hx = 0
     for page in PRank.keys():
         hx += PRank[page]*log(1/PRank[page],2)
     return 2**hx
 
+#checking for convergence
 def check_for_convergence(perp_list):
     frac1,val1 = modf(perp_list[0])
     frac2, val2 = modf(perp_list[1])
@@ -58,6 +61,7 @@ def check_for_convergence(perp_list):
     else:
         return False
 
+#calculating page rank
 def cal_pagerank():
     ite= 0
     len_dic = float(len(inlink_dict.keys()))
@@ -81,13 +85,8 @@ def cal_pagerank():
         perp_list.append(perplexity)
         print("Perplexity for iteration",ite,"is",perplexity)
         ite = ite+1
-
-
-
     #check_for_convergence
-
     while(not check_for_convergence(perp_list)):
-
         SinkPageRank = 0
         for page in sinknodes:
             SinkPageRank = float(SinkPageRank + PRank.get(page))
@@ -107,13 +106,14 @@ def cal_pagerank():
         perp_list = temp
         #dump_pagerank()
 
+#retrieve top k ranks documents
 def top_rank():
     SortedPR = sorted(PRank.iteritems(), key=operator.itemgetter(1), reverse=True)
     for i in range(50):
         print (SortedPR[i])
-
 inlink_rank = {}
 
+#retreive top k inlink ranked pages
 def top_inkink():
     #SortedInlink = sorted(inlink_dict.iteritems(), key=operator.itemgetter(1), reverse=True)
     for page in inlink_dict.keys():
